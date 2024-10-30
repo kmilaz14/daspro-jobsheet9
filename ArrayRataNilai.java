79054c2610d41463e379867f203bc95f37135978
kmilaz14 <kamilazhwa@gmail.com>
2024-10-31 07:34:28 +0800

    pertanyaan 1

--- ArrayRataNilai.java
+++ ArrayRataNilai.java
@@ -6,16 +6,22 @@ public class ArrayRataNilai {
         int[] nilaiMhs = new int[10];
         double total = 0;
         double rata2;
+        int jmlhLulus = 0;
 
         for (int i=0; i< nilaiMhs.length; i++){
             System.out.print("Masukkan nilai mahasiswa ke-"+ (i+1) + " : ");
             nilaiMhs[i]=sc.nextInt();
         }
+        
         for (int i=0; i<nilaiMhs.length; i++){
             total += nilaiMhs[i];
+            if (nilaiMhs[i]>70) {
+                jmlhLulus++;
+            }
         }
 
         rata2= total/nilaiMhs.length;
         System.out.println("Rata-rata nilai = " + rata2);
+        System.out.println("Jumlah mahasiswa yang lulus = " + jmlhLulus);
     }
 }
