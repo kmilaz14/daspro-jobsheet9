1d503638251f5bb60d4645e8039c05f56d1044db
kmilaz14 <kamilazhwa@gmail.com>
2024-10-31 06:50:17 +0800

    pertanyaan 3

--- ArrayBilangan13.java
+++ ArrayBilangan13.java
@@ -7,9 +7,8 @@ public class ArrayBilangan13 {
         bil[2]= 7.5;
         bil[3]= 2000000;
 
-        System.out.println(bil[0]);
-        System.out.println(bil[1]);
-        System.out.println(bil[2]);
-        System.out.println(bil[3]);
+        for (int i = 0; i < 4; i++) {
+            System.out.println(bil[i]);
+        }
     }
 }
