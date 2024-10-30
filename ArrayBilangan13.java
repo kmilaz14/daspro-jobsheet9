7ada69eb3919ac2ebec69402a3d4ed353aa69248
kmilaz14 <kamilazhwa@gmail.com>
2024-10-31 06:46:24 +0800

    pertanyaan 2

--- ArrayBilangan13.java
+++ ArrayBilangan13.java
@@ -1,11 +1,11 @@
 public class ArrayBilangan13 {
 
     public static void main(String[] args) {
-        int[] bil = new int[4];
-        bil[0]= 5;
-        bil[1]= 13;
-        bil[2]= -7;
-        bil[3]= 17;
+        double[] bil = new double[4];
+        bil[0]= 5.0;
+        bil[1]= 12867;
+        bil[2]= 7.5;
+        bil[3]= 2000000;
 
         System.out.println(bil[0]);
         System.out.println(bil[1]);
